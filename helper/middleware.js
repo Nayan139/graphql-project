e67f59adfffeeb362f1d232e96507779bf391dce
Nayan <nayanbhakhar7075@gmail.com>
2023-06-14 16:20:13 +0530

    code refactoring

--- helper/middleware.js
+++ helper/middleware.js
@@ -1,10 +1,10 @@
-import Jwt from 'jsonwebtoken'
+import Jwt from "jsonwebtoken";
 
 export const context = ({ req }) => {
-    const { authorization } = req.headers;
-
-    if (authorization) {
-      const { userId, email } = Jwt.verify(authorization, "Test@123")
-      return { userId, email }
-    }
+  const { authorization } = req.headers;
+  console.log(authorization);
+  if (authorization) {
+    const { userId, email } = Jwt.verify(authorization, "Test@123");
+    return { userId, email };
   }
+};
